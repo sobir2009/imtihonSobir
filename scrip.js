cdebce7e46771e67a3ec15535a7e0ebb93bcaa9b
sobir2009 <omondavlatovsobir@gmail.com>
2023-08-25 12:50:42 +0500

    sdvsd

---
+++ scrip.js
@@ -0,0 +1,166 @@
+// 2-variant  Js qachon yaratilgan va qachondan boshlab browserda va qachondan boshlab backend ishlay boshladi?
+
+// JavaScript (JS) 1995-yilning mart oyida Netscape Communications Corporation tomonidan yaratilgan. U asosan browserlar uchun sifatida yaratilgan va veb sahifalardagi interaktivlikni oshirish uchun ishlatilgan.
+// Browserlarda JavaScriptning ko'plab versiyalari mavjud bo'lib kelgan. Ushbu versiyalardan biri ECMAScript (ES) deb nomlanadi. ES6 (2015-yilda) JavaScriptning eng katta versiyasi bo'ldi va yangi qo'shimchalarni va yangiliklarni kiritdi.
+// Backend ishlash uchun JavaScript Node.js yordamida ham ishlatilishi mumkin. Node.js JavaScript dasturlash tillarini server tomonida ishlatish imkonini beradi. Node.js 2009-yilning mart oyida Ryan Dahl tomonidan ishlab chiqilgan.
+// Bundan oldin JavaScript asosan browserlarda ishlatilardi, lekin Node.js bilan backend ishlash imkoniyati paydo bo'ldi. Bu JavaScriptning server tomonida ham o'zining kuch va imkoniyatlarini namoyon etish imkoniyatini berdi.
+// Shuningdek, JavaScriptning mobil ilovalarda ham ishlatilishi mumkin, misol uchun React Native frameworki orqali. Bunda JavaScript asosida mobil ilovalar yaratish imkoniyati mavjud bo'lib, ishlovchi kodni iOS va Android ilovalariga o'girish mumkin.
+// Yani, JavaScript asosan browserlarda ishlatilgan, esa Node.js yordamida server tomonida backend ishlash uchun ham ishlatiladi.
+
+// function reverseFirstAndLastDigits(number) {
+    //     var numberString = number.toString();
+    //     var firstDigit = numberString[0];
+    //     var lastDigit = numberString[numberString.length - 1];
+    //     var middleDigits = numberString.slice(1, numberString.length - 1);
+    //     var reversedNumberString = lastDigit + middleDigits + firstDigit;
+    //     var reversedNumber = parseInt(reversedNumberString);
+    //     return reversedNumber;
+    //   }
+    //   var n = 36985;
+    //   var result = reverseFirstAndLastDigits(n);
+    //   console.log("Birinchi va oxirgi raqamlar almashtirilgan raqam: " + result);
+    
+    
+    
+    
+    
+    
+    
+    // function hafta(a){
+    //     switch (a){
+            
+    //     case 1:
+    //             // return dushanba;
+    //     break;
+    //     case 2:
+    //     return seshanba;
+    //     break;
+    //     case 3:
+    //     return chorshanba;
+    //     break;
+    //     case 4:
+    //     return payshanba;
+    //     break;
+    //     case 5:
+    //     return juma;
+    //     break;
+    //     case 6:
+    //     return shanba;
+    //     break;
+    //     case 7:
+    //     return yakshanba;
+    //     break
+    //     default:
+    //     return unday kun yoq;
+    //     break
+    //     }
+    // }
+    // console.log(hafta(11));
+    
+    
+    
+    
+    // function hafta(a){
+    //     switch (a){
+            
+    //     case 1:
+    //     return yomon;
+    //     break;
+    //     case 2:
+    //     return qoniqarsiz;
+    //     break;
+    //     case 3:
+    //     return qoniqarli;
+    //     break;
+    //     case 4:
+    //     return yaxshi;
+    //     break;
+    //     case 5:
+    //     return A'lo;
+    //     break;
+    //     default:
+    //     return Error
+    //     break;
+        
+        
+        
+    // }
+    
+    // }
+    // console.log(hafta(5));
+    
+    
+    // 
+    // function kun(A){
+    
+    //     switch (A){
+            
+    //             case 1,3,5,7,9,11:
+    //             return 31kun bor;
+    //             break;
+    //             case 4,6,8,10,12:
+    //             return 31 kun bor;
+    //             case 2:
+    //             return 28 4yilda bir 29;
+    //             break;
+    //             default :
+    //             return unday kun yoq;
+    //             break;
+    
+    
+    
+    //     }function oy(A){
+    
+    //     switch (A){
+            
+    //             case 1,2,12:
+    //             return qish;
+    //             break;
+    //             case 3,4,5:
+    //             return bahor;
+    //             break;
+    //             case 6,7,8:
+    //             return yoz;
+    //             break;
+    //             default:
+    //             return kuz;
+    //             break
+    //     }
+    
+    // console.log(oy(12));
+    
+    
+    //     }
+    // console.log(kun(20));
+        
+    
+    
+    
+    // function sonlar(a){
+    // for (let i=1;i<=a;i++){
+    //     console.log(i);
+    // }
+    // }
+    // console.log(sonlar(20));
+    
+    
+    
+    
+    // Kun boshidan boshlab N sekund o'tdi.
+    //  Kun boshidan boshlab qancha soat va sekund o'tganini hisoblovchi 
+    //  dastur tuzing. Masalan, kun boshidan N=12321 sekund o'tdi. Bu kun 
+    //  boshidan boshlab 3 soat 1521 sekund degani.
+    // function sekund(s){
+    //     let soat=s/3600;
+    //     soat=parseInt(soat);
+    //     if (soat>60){
+    //         return bu kun;
+    //     }
+    //     else{
+    //         return soat;
+    //     }
+    // }
+    // console.log(sekund(3661));
+    
+    
+    
